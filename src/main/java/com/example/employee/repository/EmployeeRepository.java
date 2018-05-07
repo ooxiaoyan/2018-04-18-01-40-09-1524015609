9917740e8dbebdbc3197670494fecffe290d1b89
ooxiaoyan <810978527@qq.com>
2018-05-07 21:03:39 +0800

    feat(repository): add EmployeeRepository

--- src/main/java/com/example/employee/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/repository/EmployeeRepository.java
@@ -1,7 +1,11 @@
 package com.example.employee.repository;
 
 import com.example.employee.entity.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -9,16 +13,21 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     //以下所有的*都代表变量
 
     //1.查询名字是*的第一个employee
-
+    Employee findByName(String name);
     //2.找出Employee表中第一个姓名包含`*`字符并且薪资大于*的雇员个人信息
-
+    Employee findByNameLikeAndSalaryGreaterThan(String word, Integer salary);
     //3.找出一个薪资最高且公司ID是*的雇员以及该雇员的姓名
-
+    Employee findTopByCompanyIdOrderBySalaryCompanyIdDesc(Integer id);
     //4.实现对Employee的分页查询，每页两个数据
-
+    @Override
+    Page<Employee> findAll(Pageable pageable);
     //5.查找**的所在的公司的公司名称
-
+    @Query("select c.companyName from Employee e left join Company c on e.companyId = c.id where e.name = ?1")
+    String findCompanyNameByEmployeeName(String name);
     //6.将*的名字改成*,输出这次修改影响的行数
-
+    @Modifying
+    @Query("update Employee set name = ?2 where name = ?1")
+    Integer updateByName(String oldName, String newName);
     //7.删除姓名是*的employee
+    void deleteByName(String name);
 }
