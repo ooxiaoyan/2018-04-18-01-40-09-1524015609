644c6b1b81017f926456ecc20a972e81cb8f0f1b
ooxiaoyan <810978527@qq.com>
2018-05-07 21:02:18 +0800

    feat(entity): add Company

--- src/main/java/com/example/employee/entity/Company.java
+++ src/main/java/com/example/employee/entity/Company.java
@@ -1,5 +1,39 @@
 package com.example.employee.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Company {
 
+    @Id
+    @GeneratedValue
+    private int id;
+    private String companyName;
+    private String employeesNumber;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getEmployeesNumber() {
+        return employeesNumber;
+    }
+
+    public void setEmployeesNumber(String employeesNumber) {
+        this.employeesNumber = employeesNumber;
+    }
 }
